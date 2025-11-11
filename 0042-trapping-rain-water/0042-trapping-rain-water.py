c7933a765bc2a63ae4056ac7c7f5e15bf78eb97c
Jaeyun <76769946+Jaeyun-Moon@users.noreply.github.com>
2025-11-11 19:02:44 +0900

    Time: 25 ms (29.93%), Space: 19.8 MB (7.56%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,21 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        stack = [] 
+        volume = 0 
+        
+        for ind,h in enumerate(height):
+            print(h)
+            while stack and height[stack[-1]] < h:
+                
+                prev_ind = stack.pop() 
+                
+                if not stack:
+                    break 
+                # 거리 
+                s = ind - stack[-1] - 1 
+                # 높이
+                he = min(height[stack[-1]], h) - height[prev_ind]
+                volume += s * he
+
+            stack.append(ind)
+        return volume
