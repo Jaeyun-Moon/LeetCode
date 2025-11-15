b6257117ea8819ad4797c660290be1006641dd38
Jaeyun <76769946+Jaeyun-Moon@users.noreply.github.com>
2025-11-16 00:55:24 +0900

    Time: 3 ms (87.51%), Space: 18.5 MB (53.12%) - LeetHub

--- 0860-design-circular-queue/0860-design-circular-queue.py
+++ 0860-design-circular-queue/0860-design-circular-queue.py
@@ -1,39 +1,36 @@
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        self.circle = [None] * k 
-        self.max_len = k 
-        self.first = 0 
-        self.rear = 0  
+        self.circle = [None] * k
+        self.head, self.rear = 0,0 
+        self.max_len = k
 
     def enQueue(self, value: int) -> bool:
-        if self.circle[self.rear] is None:
+        if self.circle[self.rear] == None:
             self.circle[self.rear] = value 
-            # 이동 
-            self.rear = (self.rear + 1) % self.max_len
-            return True 
+            self.rear = (self.rear+1) % self.max_len
+            return True
         else:
             return False 
     def deQueue(self) -> bool:
-        if self.circle[self.first] is not None:
-            self.circle[self.first] = None 
-            self.first = (self.first + 1) % self.max_len
-            return True 
-        else:
+        if self.circle[self.head] == None:
             return False
+        else:
+            self.circle[self.head] = None
+            self.head = (self.head + 1 )%self.max_len
+            return True 
 
     def Front(self) -> int:
-        return -1 if self.circle[self.first] is None else self.circle[self.first]
+        return -1 if self.circle[self.head] is None else self.circle[self.head]
 
     def Rear(self) -> int:
         return -1 if self.circle[self.rear-1] is None else self.circle[self.rear-1]
 
     def isEmpty(self) -> bool:
-        return self.first == self.rear and self.circle[self.first] is None 
+        return self.head == self.rear and self.circle[self.head] is None 
 
     def isFull(self) -> bool:
-        return self.first == self.rear and self.circle[self.first] is not None 
-        
+        return self.rear == self.head and self.circle[self.head] is not None 
 
 
 # Your MyCircularQueue object will be instantiated and called as such:
