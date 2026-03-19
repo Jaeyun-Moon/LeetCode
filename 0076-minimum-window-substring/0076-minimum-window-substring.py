d968ceb8630d219a2bc6a63588677c362221c443
Jaeyun <76769946+MoonRascal-Code@users.noreply.github.com>
2026-03-19 14:23:56 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -9,7 +9,7 @@ class Solution:
             # 1, A 
             missing -= need[char]>0 
             need[char] -= 1
-        
+            print(missing)
             if missing == 0:
                 while left < right and need[s[left]] < 0 :
                     need[s[left]] +=1 
